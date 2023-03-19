3ea2b5b044931a297f58990ac07a834d242d97bf
jessica14angulo <ang21016@byui.edu>
2023-03-18 23:34:54 -0400

    Week 11

--- src/app/messages/message-item/message-item.component.ts
+++ src/app/messages/message-item/message-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from 'src/app/contacts/contact.model';
 import { ContactService } from 'src/app/contacts/contact.service';
 import { Message } from '../message.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-message-item',
@@ -9,17 +10,22 @@ import { Message } from '../message.model';
   styleUrls: ['./message-item.component.css']
 })
 export class MessageItemComponent implements OnInit {
-  messageSender: string = "";
+  messageSender: string;
   @Input() message: Message;
+  sub: Subscription
 
   constructor(private contactService: ContactService) { 
 
   }
 
   ngOnInit() {
-    this.contactService.getContact(this.message.sender)
+    this.sub = this.contactService.getContact(this.message.sender)
       .subscribe(contactData => {
         this.messageSender = contactData.contact.name;
       })
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
